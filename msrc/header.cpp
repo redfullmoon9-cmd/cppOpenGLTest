19ab77eef1cc630e359c09d135bb82bb5c262559
redfullmoon9-cmd <redfullmoon9@gmail.com>
2026-03-07 15:09:32 +0900

    Complete :functionality

--- msrc/header.cpp
+++ msrc/header.cpp
@@ -2,12 +2,13 @@
 
 std::optional<std::string> Header::myLoadTextFile(const std::string &fileName)
 {
-    std::fstream fileIn(fileName); 
+    SPDLOG_INFO(" read shader file {}", fileName);  
+    std::ifstream fileIn(fileName); 
     if(!fileIn.is_open()){
         SPDLOG_ERROR(" File open error {}", fileName); 
         return {}; //함수반환해야 하는 기본 타입을 반환 여기서 optinal은 아무것도 없는(empty) 값이 됨. 
     }
     std::stringstream text; 
     text << fileIn.rdbuf(); 
-    return text.str(); //파일을 열었아면 문자열을 리턴한다. 
+    return text.str(); //파일을 열었다면 문자열을 리턴한다. 
 }
--- msrc/header.h
+++ msrc/header.h
@@ -1,10 +1,21 @@
 #ifndef __HEADER_H__
 #define __HEADER_H__
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <optional>
+#include <string>
+#include <memory>
+#include <fstream>
+#include <sstream>
+#include "spdlog/spdlog.h"
 
 class Header{
     public :
+    // KShader/simple.fs 로컬파일을 리딩하는 목적의 공통함수. 
+    // optinal은 메모리 동적 할당시에 자동적으로 메모리 해제를 담당하게 해주는 역활.
+    // 리턴 타입으로 스트랑 타입의 포인터로 받지만 해제시킬 필요가 없는 포인터를 제공
+    std::optional<std::string> myLoadTextFile(const std::string& filename); 
 
     private:
 }; 
