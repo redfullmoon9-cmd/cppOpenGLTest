127f62519ebe345dec2b4cf4b9f4268ee97badd9
redfullmoon9-cmd <redfullmoon9@gmail.com>
2026-03-07 15:06:54 +0900

    refacting : attatch code referacnce

--- src/context.cpp
+++ src/context.cpp
@@ -10,15 +10,17 @@ std::unique_ptr<Context>  Context::Create()
     return std::move(context); 
 }
 
+//attribut변경. position color 
+//이전 참조코드 InitRef로 보내고 코드 정리. 
 bool Context::Init()
 {
 
     float vertices[]={
         //삼각형 두개를 합처서 그린다. 첫번째 사각형. 
-        0.5f, 0.5f, 0.0f,  //top right
-        0.5f, -0.5f, 0.0f,   //bottom right
-        -0.5f, -0.5f, 0.0f,  //bottem left
-        -0.5f, 0.5f, 0.0f  //top left 
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    //top right red, 
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,    //bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, //bottem left
+        -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f //top left  yellow 
     }; 
 
     uint32_t indices[] ={
@@ -26,43 +28,81 @@ bool Context::Init()
         1, 2, 3  // second 
     }; 
 
-    // glGenVertexArrays(1, &m_vertexArrayObject); //VAO 생성. -- vertexLayout으로 
-    // glBindVertexArray(m_vertexArrayObject); //사용할 VAO 설정. 
-    m_vertexlayout =VertexLayout::Create(); 
+    m_vertexlayout =VertexLayout::Create(); //VAO 설정 
 
+    //vertices를 buffer에 복사.  
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float)*24); 
 
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float)*12); 
+    // m_vertexlayout->setAttribute(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0); //attribue 를 1개에서 2개를 설정하는 것으로 변경. 
+    // 몇번째 attriute 인가                   노말라이즈여부  건널뛸 바이트    초기 옵셋값
+    m_vertexlayout->setAttribute(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*6, 0); //attribue 0번 설정. 
+    m_vertexlayout->setAttribute(1, 3, GL_FLOAT, GL_FALSE, sizeof(float)*6, sizeof(float)*3); //attribue 1번 설정. 
 
-    /* buffer 클래스 refactoring
-    glGenBuffers(1, &m_vertexBuffer); //VBO
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer); 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)* 12, vertices, GL_STATIC_DRAW); //지정된 버퍼에 데이터를 복사. 
-    */
-
-    //(N) 정점 attribute중 n번째를 사용하도록 설정. KShaer/simple.vs 
-    //ayout (location = 0) in vec3 aPos; 내에 location =0 의 0에 해당하는 값
-
-    // glEnableVertexAttribArray(0); 
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0); 
-    m_vertexlayout->setAttribute(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0); 
-
-    
-    /** index buffer --> buffer class   
-     glGenBuffers(1, &m_indexBuffer); 
-     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer); 
-     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t)*6, indices, GL_STATIC_DRAW); 
-     */
-   m_indexBuffer =Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t)*6); 
+    m_indexBuffer =Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t)*6); 
 
 
     //cmakelists.txt에 구성되어 있는 파일의 위치에서 읽어 오는 방식으로 수정. 
     // -- 갱신시에 build패스에 있는 KShader을 참조하는 문제 때문에..
-    std::shared_ptr<KShader> vertexShader = KShader::CreateFromFile(SHADER_DIR "simple.vs", GL_VERTEX_SHADER); 
-    std::shared_ptr<KShader> fragmentShader = KShader::CreateFromFile(SHADER_DIR "simple.fs", GL_FRAGMENT_SHADER);
+    std::shared_ptr<KShader> vertexShader = KShader::CreateFromFile(SHADER_DIR "per_vertex_color.vs", GL_VERTEX_SHADER); 
+    std::shared_ptr<KShader> fragmentShader = KShader::CreateFromFile(SHADER_DIR "per_vertex_color.fs", GL_FRAGMENT_SHADER);
 
     //create가 성공하면 포인터로 값이 전달되고 없다면 nullptr이 들어 있다. 
-    // std::shared_ptr<KShader> vertexShader = KShader::CreateFromFile("KShader/simple.vs", GL_VERTEX_SHADER); 
-    // std::shared_ptr<KShader> fragmentShader =KShader::CreateFromFile("KShader/simple.fs", GL_FRAGMENT_SHADER); 
+    if(!vertexShader || !fragmentShader ){
+        return false; 
+    } 
+    
+    SPDLOG_INFO("vertex shader id {}", vertexShader->Get()); // 포인터 주소 -->
+    SPDLOG_INFO("fragment shader id {}", fragmentShader->Get()); 
+    
+    m_program =Program::Create({fragmentShader, vertexShader }); 
+    if(!m_program){
+        return false; 
+    }
+    SPDLOG_INFO("program id :{}", m_program->Get()); 
+    glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
+    
+    return true;
+}
+
+bool Context::InitRef()
+{
+    float vertices[]={
+        0.5f, 0.5f, 0.0f, 
+        0.5f, -0.5f, 0.0f, 
+        -0.5f, -0.5f, 0.0f 
+    }; //일단 
+
+    uint32_t indices[] ={ //사각형을 두개의 삼각형으로 그리는 예제에 필요함. 
+        0, 1, 3, // first trialge 
+        1, 2, 3  // second 
+    }; 
+
+    glGenVertexArrays(1, &m32_vertexArrayObject); //VAO 생성. 
+    glBindVertexArray(m32_vertexArrayObject); //사용할 VAO 설정. 
+
+    glGenBuffers(1, &m32_vertexBuffer); //VBO
+    glBindBuffer(GL_ARRAY_BUFFER, m32_vertexBuffer); 
+    //vertices 배열을 buffer에 복사.  
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)* 12, vertices, GL_STATIC_DRAW); //위에서 지정한 VBO 버퍼에 데이터를 복사. 
+    
+
+    //(N) 정점 attribute중 n번째를 사용하도록 설정. KShaer/simple.vs 
+    //ayout (location = 0) in vec3 aPos; 내에 location =0 의 0에 해당하는 값
+    glEnableVertexAttribArray(0); 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0); 
+
+    /** index buffer --> buffer class   */
+     glGenBuffers(1, &m32_indexBuffer); //인덱스 버퍼를 생성. 
+     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m32_indexBuffer); //별도로 인덱스 버퍼를 바인딩
+     //인덱스 버퍼를 사용해서 두개의 삼각형의 공통지점으로 그린다ㅑ. 
+     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t)*6, indices, GL_STATIC_DRAW); 
+     
+
+    //cmakelists.txt에 구성되어 있는 파일의 위치에서 읽어 오는 방식으로 수정. 
+    // -- 갱신시에 build패스에 있는 KShader을 참조하는 문제 때문에..
+    //create가 성공하면 포인터로 값이 전달되고 없다면 nullptr이 들어 있다. 
+    std::shared_ptr<KShader> vertexShader = KShader::CreateFromFile("KShader/simple.vs", GL_VERTEX_SHADER); 
+    std::shared_ptr<KShader> fragmentShader =KShader::CreateFromFile("KShader/simple.fs", GL_FRAGMENT_SHADER); 
     
     if(!vertexShader || !fragmentShader ){
         return false; 
@@ -77,11 +117,10 @@ bool Context::Init()
     }
     SPDLOG_INFO("program id :{}", m_program->Get()); 
 
-    //쉐이더 파일에서 uniform 값을 읽어옴. 
-    auto loc =glGetUniformLocation(m_program->Get(),"color" ); //sinple.fs의 uniform 변수의 이름. 
+    //쉐이더 파일에서 uniform 값을 읽어옴. -- 쉐이더 파일 변경. 
+    auto loc =glGetUniformLocation(m_program->Get(),"color" ); //simple.fs의 uniform 변수의 이름. 
     m_program->Use();
     glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);  //color란 변수가 있는 곳에 loc에 float 값 1.0f, 1.0f ... 등의 값을 입력  
-
 
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
     
@@ -91,21 +130,32 @@ bool Context::Init()
 void Context::Render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    // glPointSize(10.f); // 포인트 크기를 
-    // glUseProgram(m_program->Get()); 
+    
+    m_program->Use(); 
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //정점 4개와 인덱스를 이용한 사각형. 
+
+}
+
+//참조용 코드. 
+void Context::RenderRef()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    // glPointSize(10.f); // 점찍기 포인트 크기를 크게 하기. 
+    // glUseProgram(m_program->Get()); --> Program class 로 변경. 
  
+    /** animation start: 시간을 이용해서 RGB값이 변경되는 방식으로 애니메이션 구현*/
     static float time =0.0f ;
-    float t= sinf(time) *0.5f + 0.5f; 
+    float t= sinf(time) *0.5f + 0.5f; //0에서 1 사이의 값을 얻기 위해서
     auto loc =glGetUniformLocation(m_program->Get(), "color"); 
     m_program->Use(); 
     glUniform4f(loc, t*t, 2.0f *t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f); 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //정점 4개와 인덱스를 이용한 사각형. 
-
     time += 0.016f; 
-    // glDrawArrays(GL_POINTS, 0, 1);  
-    // glDrawArrays(GL_TRIANGLES, 0, 3);  
+    /*animation  end;  */
+
+    // glDrawArrays(GL_POINTS, 0, 1);  //점그리기 
+    // glDrawArrays(GL_TRIANGLES, 0, 3);  // 삼각형 그리기 
     // glDrawArrays(GL_LINE_STRIP, 0, 4);  //점 4개를 연결한 삼각형. 
     // glDrawArrays(GL_TRIANGLES, 0, 6);//점 6개를 그려서 삼각형 2개를 그린다.   
-
 
 }
--- src/context.h
+++ src/context.h
@@ -7,14 +7,24 @@
 #include "vertex_layout.h"
 
 // CLASS_PTR(Context)
+
+/** openGL 함수를 호출 하는 context를 담고 있음. 
+ *  1. VAO생성과 설정. 
+ *  2. ArrayBuffer 생성 설정... 
+ *  3. program 
+ *  4. shader 생성
+ */
 class Context{
     public:
         static std::unique_ptr<Context> Create(); 
-        void Render(); 
+        void Render();
+        void RenderRef(); //참조용 코드  
         ~Context(){}
     private:
         Context(){}
         bool Init(); 
+        bool InitRef(); //참조용 코드. 
+
         // ProgramUPtr m_program; 눈에 잘 않들어와서 길게 쓰기로 . 
         std::unique_ptr<Program> m_program; 
 
@@ -26,6 +36,11 @@ class Context{
 
         // uint32_t m_vertexArrayObject; -- vertexLayout 클래스로 대체 
         std::unique_ptr<VertexLayout> m_vertexlayout; 
+
+        //참조용 코드를 위한 변수 -- 사용하지 않음. 에러 방지. 
+        uint32_t m32_vertexArrayObject{0}; 
+        uint32_t m32_vertexBuffer{0}; 
+        uint32_t m32_indexBuffer{0}; 
 }; 
 
 #endif //__CONTEXT_H__
